53dcb0dd391408764f843d78566f4f131f6cdb84
Beep2049 <155083633+Beep2049@users.noreply.github.com>
2024-11-20 09:12:29 -0500

    Update player.py

--- Space Invaders/player.py
+++ Space Invaders/player.py
@@ -4,7 +4,7 @@ from laser import Laser
 class Player(pygame.sprite.Sprite):
   def __init__(self,pos, constraint, speed):
     super().__init__()
-    self.image = pygame.image.load(r"C:\The Tings\Projects\personal_website\Website Projects\Space Invaders\graphics_sp\player.png").convert_alpha()
+    self.image = pygame.image.load(r"...\graphics_sp\player.png").convert_alpha()
     self.rect = self.image.get_rect(midbottom = pos)
     self.speed = speed
     self.max_x_constraint = constraint
@@ -14,7 +14,7 @@ class Player(pygame.sprite.Sprite):
 
     self.lasers = pygame.sprite.Group()
 
-    self.laser_sound = pygame.mixer.Sound(r"C:\The Tings\Projects\personal_website\Website Projects\Space Invaders\sounds_sp\audio_laser.wav")
+    self.laser_sound = pygame.mixer.Sound(r"...\sounds_sp\audio_laser.wav")
     self.laser_sound.set_volume(0.5)
     
   def get_input(self):
@@ -29,7 +29,7 @@ class Player(pygame.sprite.Sprite):
       self.shoot_laser()
       self.ready = False
       self.laser_time = pygame.time.get_ticks()
-      self.laser_sound = pygame.mixer.Sound(r"C:\The Tings\Projects\personal_website\Website Projects\Space Invaders\sounds_sp\audio_laser.wav")
+      self.laser_sound = pygame.mixer.Sound(r"...\sounds_sp\audio_laser.wav")
 
   def recharge(self):
     if not self.ready:
